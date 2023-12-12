5f2ae90871decdc0364453e075df10039c245b22
Arthur Duarte <61255233+arthurdpcm@users.noreply.github.com>
2023-12-12 13:26:08 -0300

    Correct tp3.py

--- CMB/tp3.py
+++ CMB/tp3.py
@@ -3,38 +3,43 @@ import sys
 import matplotlib.pyplot as plt
 
 def generate_random_sequence(n):
-  return [random.random() for _ in range(n)]
+    return [random.random() for _ in range(n)]
 
 def plot_sequence(n):
-  xn = generate_random_sequence(n)
-  fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-  axs[0].plot(range(n), xn, 'o')
-  axs[0].set_xlabel('n')
-  axs[0].set_ylabel('xn')
-  axs[1].plot(xn[:-1], xn[1:], 'o')
-  axs[1].set_xlabel('xn-1')
-  axs[1].set_ylabel('xn')
-  axs[2].hist(xn, bins=20)
-  axs[2].set_xlabel('xn')
-  axs[2].set_ylabel('Frequency')
-  plt.show()
-  return xn
+    xn = generate_random_sequence(n)
+    sorted_xn = sorted(xn)  # Sort the sequence
+
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    
+    axs[0].scatter(range(n), xn, s=10, color='red') 
+    axs[0].plot(range(n), xn, '-', label='Random Sequence') 
+    
+    axs[0].set_xlabel('n')
+    axs[0].set_ylabel('xn')
+
+    
+    axs[1].scatter(xn[:-1], xn[1:], color='orange')
+    axs[1].set_xlabel('xn-1')
+    axs[1].set_ylabel('xn')
+    
+    axs[2].hist(xn, bins=20, color='purple')
+    axs[2].set_xlabel('xn')
+    axs[2].set_ylabel('Frequency')
+    
+    plt.show()
+    return xn
+
 def main():
-  n = int(sys.argv[1])
-  xn = plot_sequence(n)
+    n = int(sys.argv[1])
+    xn = plot_sequence(n)
 
-  mean = sum(xn) / len(xn)
-  median = sorted(xn)[len(xn) // 2]
-  mode = max(xn, key=xn.count)
+    mean = sum(xn) / len(xn)
+    median = sorted(xn)[len(xn) // 2]
+    mode = max(xn, key=xn.count)
 
-  print("Mean:", mean)
-  print("Median:", median)
-  print("Mode:", mode)
+    print("Mean:", mean)
+    print("Median:", median)
+    print("Mode:", mode)
 
 if __name__ == '__main__':
-  main()
-
-#The code generates a sequence of random numbers and plots them in three different ways.
-# Each time the code is run,
-# a different sequence of random numbers is generated and plotted.
-# Therefore, the output will be different each time the code is run.
+    main()
